71f661986ee82f93c3bb6499e8c443fb73b1550e
Adrián Bedón <arbedon2002@gmail.com>
2024-01-06 10:49:51 -0500

    Cambios de defensa implementados

--- viewsApiViajes/src/pages/ReportPage.jsx
+++ viewsApiViajes/src/pages/ReportPage.jsx
@@ -1,10 +1,33 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ReportContext } from "../context/ReportContext";
 import { PeakList } from "../components/PeakList";
 import { OffList } from "../components/OffList";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 export const ReportPage = () => {
-  const { peaks, getPeaks, offs, getOffs } = useContext(ReportContext);
+  const [endingDate, setEndingDate] = useState(new Date());
+  const [initialDate, setInitialDate] = useState(new Date());
+  const [sale, setSale] = useState(0);
+
+  const { peaks, getPeaks, offs, getOffs, sales } = useContext(ReportContext);
+
+  const fetchSale = async () => {
+    try {
+      const { data } = await axios.post(
+        "https://core-viajes.onrender.com/peak/reportSale",
+        {
+          startDate: initialDate,
+          endDate: endingDate,
+        }
+      );
+
+      setSale(data);
+    } catch (error) {
+      console.error("Error retrieving sales", error);
+    }
+  };
 
   useEffect(() => {
     getPeaks();
@@ -41,6 +64,46 @@ export const ReportPage = () => {
               <OffList />
             )}
           </div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="col">
+            <div className="input-group mb-3">
+              <span className="input-group-text">Fecha Inicial</span>
+              <DatePicker
+                type="date"
+                name="fechaInicio"
+                id="fechaInicio"
+                selected={initialDate || ""}
+                className="form-control"
+                onChange={(date) => setInitialDate(date)}
+                dateFormat={"yyyy-MM-dd"}
+              ></DatePicker>
+            </div>
+          </div>
+          <div className="col">
+            <div className="input-group mb-3">
+              <span className="input-group-text">Fecha Final</span>
+              <DatePicker
+                type="date"
+                name="fechaRetorno"
+                id="fechaRetono"
+                selected={endingDate || ""}
+                className="form-control"
+                onChange={(date) => setEndingDate(date)}
+                dateFormat={"yyyy-MM-dd"}
+              ></DatePicker>
+            </div>
+          </div>
+          <div className="col">
+            <button
+              type="button"
+              className="btn btn-create"
+              onClick={() => fetchSale()}
+            >
+              Filtrar
+            </button>
+          </div>
+          <p>La ganancia total neta para el rango de fechas seleccionada es: ${sale}</p>
         </div>
       </div>
     </>
